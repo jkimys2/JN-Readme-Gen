b9cdfd59104daf6f1659562fde9c37b3fbf4a30f
Jordan <jkimys2@gmail.com>
2024-01-23 11:12:56 -0500

    file push, questions update

--- index.js
+++ index.js
@@ -1,7 +1,80 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const genMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("You must enter your GitHub username!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your email:",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("You must enter a valid email address!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project title name?",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please provide a name for your project!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter a description for your project:",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log(
+            "Your project needs a description! Please enter a description."
+          );
+          return false;
+        }
+      },
+    },
+  ]),
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
